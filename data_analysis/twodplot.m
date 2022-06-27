4b40a5c8722ea88f1bd26ce0c0bda7b8e558e41e
NivisPluma <matzeminecraft@gmail.com>
2022-06-27 18:13:05 +0200

    changed Plot

--- data_analysis/twodplot.m
+++ data_analysis/twodplot.m
@@ -2,14 +2,8 @@ rawdata = readmatrix('results.csv');
 xA = rawdata(:,7);
 yA = rawdata(:,5);
 scatter(xA,yA)
-
-counter = 0;
-limit = 20; %change height of horizontal line here
-for i = 1:size(xA)
-    if xA(i,1) > limit
-        counter = counter +1;
-    end
-end
-
-counter
-yline(limit)
+hold on
+x = [5 10 15 20 25 30 35 40 100];
+% jetzt nur noch die richtige Skala einstellen
+yP=2.^x.^(1/3);
+plot(x,yP)
